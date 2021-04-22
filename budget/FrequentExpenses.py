d68906e4209a748950557616f446c6fec86b3c32
Aveshen17 <81326165+Aveshen17@users.noreply.github.com>
2021-04-22 19:38:18 +0200

    Completed module

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -3,4 +3,6 @@ from . import Expense
 expenses = Expense.Expenses()
 expenses.read_expenses(data/spending_data.csv)
 
-spending_categories=List[]
+list spending_categories=[]
+expense = iter(spending_categories)
+for expense in spending_categories
