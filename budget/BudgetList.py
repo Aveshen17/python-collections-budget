af27b57ee53d29fd508da473749958334c649910
Aveshen17 <81326165+Aveshen17@users.noreply.github.com>
2021-04-22 20:42:30 +0200

    Completed module

---
+++ budget/BudgetList.py
@@ -0,0 +1,18 @@
+
+
+class BudgetList:
+    def BudgetList(self, budget):
+        self.budget=budget
+        self.sum_expenses=0
+        self.expenses=[]
+        self.sum_overages=0
+        self.overages=[]
+
+    def append(self, item):
+        check=self.sum_expenses+item
+        if check check<self.budget:
+            self.expenses.append(item
+            self.sum_expenses+=item
+
+    def __len__(self)
+        
